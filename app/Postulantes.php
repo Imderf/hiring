a6af5c95395285b0e8dd1dc25bd530a742a6e213
gabrielgomez20132 <gabrielgomez_100@hotmail.com>
2021-12-06 12:21:22 -0300

    modelo postulantes

--- app/Postulantes.php
+++ app/Postulantes.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Postulantes extends Model
 {
-    //
+    protected $table = 'curriculum';
+    protected $primaryKey = 'id';
+    
+    protected $fillable = [ 
+
+        'nombre_completos', 
+        'email', 
+        'telefono', 
+        'fecha_nacimiento', 
+        'direccion',
+        'ciudad',
+        'paises_id',
+        'years_experiencia',
+        'otros_estudios',
+        'archivo_cv',
+        'url_linkedin',
+        'informacion_adicional',
+        'acepto_condiciones',
+        'cargo_id',
+        'vacante_id'
+
+    ];
 }
